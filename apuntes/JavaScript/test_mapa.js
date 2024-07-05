e266cd01c85469b89f0d1ce71e88269baf9f17c0
Nicolás Medina Peña <nicomedinap@gmail.com>
2024-07-05 00:28:12 -0400

    Create test_mapa.js

---
+++ apuntes/JavaScript/test_mapa.js
@@ -0,0 +1,19 @@
+const mapData = {
+    map: [
+        // Mapa de 2D aquí
+        [1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 1, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1]
+    ],
+    minimap: {
+        width: 100,
+        height: 100
+    },
+    player: {
+        x: 3,
+        y: 2,
+        angle: 0
+    }
+};
