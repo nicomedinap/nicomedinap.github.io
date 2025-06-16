7a19bdf1dc481119c46ac2ad01601408e911497b
Nicolás Medina Peña <nicomedinap@gmail.com>
2025-06-15 22:13:42 -0400

    Update CalleLarga.js

--- apuntes/JavaScript/CalleLarga.js
+++ apuntes/JavaScript/CalleLarga.js
@@ -19,10 +19,36 @@ var mapData = {
     [1,0,0,0,1],
     [1,0,0,0,1],
     [1,0,0,0,1],
-    [1,1,3,1,1]
+    [1,4,3,6,1]
   ],
   lenses: [
     { x: 2.5, y: 5.5, strength: 0.013, radius: 11.33, visible: true },
     { x: 2.5, y: 15.5, strength: 0.004, radius: 15, visible: true }
-  ]
+  ],
+  roomTextures: {
+      "2": {
+          "title": "NGC 3132", "description": "Nebulosa planetaria",
+          "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/JWST/NGC3132/201.jpg"
+      },
+      "3": {
+          "title": "NGC 1087", "description": "Galaxia espiral",
+          "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/JWST/NGC1087/012.jpg"
+      },
+      "4": {
+          "title": "NGC 3132", "description": "Nebulosa planetaria",
+          "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/HST/ARP142/120.jpg"
+      },
+      "5": {
+          "title": "NGC 3132", "description": "Nebulosa planetaria",
+          "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/JWST/NGC3132/120.jpg"
+      },
+      "6": {
+          "title": "NGC 3132", "description": "Nebulosa planetaria",
+          "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/JWST/NGC3132/201.jpg"
+      },
+      "7": {
+          "title": "NGC 3132", "description": "Nebulosa planetaria",
+          "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/JWST/NGC3132/201.jpg"
+      }
+  }
 };
