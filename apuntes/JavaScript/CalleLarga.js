fdd708cc66a3ef29431cf0350b02f61b89b518c4
Nicolás Medina Peña <nicomedinap@gmail.com>
2025-06-20 13:02:09 -0400

    Update CalleLarga.js

--- apuntes/JavaScript/CalleLarga.js
+++ apuntes/JavaScript/CalleLarga.js
@@ -25,4 +25,30 @@ var mapData = {
     { x: 2.5, y: 5.5, strength: 0.013, radius: 11.33, visible: true },
     { x: 2.5, y: 15.5, strength: 0.004, radius: 15, visible: true }
   ],
+  roomTextures: {
+      "2": {
+          "title": "NGC 3132", "Material con densidad $\\rho = 2.33\\ g/cm^3$",
+          "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/JWST/NGC3132/201.jpg"
+      },
+      "3": {
+          "title": "NGC 1087", "description": "Galaxia espiral",
+          "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/JWST/NGC1087/012.jpg"
+      },
+      "4": {
+          "title": "NGC 3132", "description": "Nebulosa planetaria",
+          "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/HST/ARP142/120.jpg"
+      },
+      "5": {
+          "title": "NGC 3132", "description": "Nebulosa planetaria",
+          "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/JWST/NGC3132/120.jpg"
+      },
+      "6": {
+          "title": "NGC 3132", "description": "Nebulosa planetaria",
+          "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/JWST/NGC3132/201.jpg"
+      },
+      "7": {
+          "title": "NGC 3132", "description": "Nebulosa planetaria",
+          "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/JWST/NGC3132/201.jpg"
+      }
+  }
 };
