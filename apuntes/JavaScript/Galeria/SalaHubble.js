0fb452d10f9536be8a1bf2b728141f04bec5579f
Nicolás Medina Peña <nicomedinap@gmail.com>
2025-06-27 13:40:47 -0400

    Create SalaHubble.js

---
+++ apuntes/JavaScript/Galeria/SalaHubble.js
@@ -0,0 +1,90 @@
+var mapData = {
+  map = [
+      [1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1],
+      [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3],
+      [1, 0, 0, 0, 0, 2, 3, 0, 0, 0, 0, 1],
+      [2, 0, 0, 0, 0, 3, 2, 0, 0, 0, 0, 3],
+      [1, 1, 2, 1, 0, 0, 0, 0, 1, 3, 1, 1],  
+      [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+      [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 1, 4, 1, 0, 0, 0, 0, 1, 5, 1, 1],  
+      [4, 0, 0, 0, 0, 4, 5, 0, 0, 0, 0, 5],
+      [1, 0, 0, 0, 0, 5, 4, 0, 0, 0, 0, 1],
+      [4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5],
+      [1, 0, 0, 0, 0, 4, 5, 0, 0, 0, 0, 1],
+      [1, 1, 4, 1, 0, 5, 4, 0, 1, 5, 1, 1],  
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],  
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
+      [1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
+      [1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
+      [1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]   
+  ],
+  sprites: [
+    {type: "hubble",x: 4, y: 3, img: null,
+      imageUrl: "https://www.pngplay.com/wp-content/uploads/13/Black-Hole-PNG-Background.png"} 
+  ],
+  "roomTextures": {
+    "2": {
+      "title": "Galaxia de las antenas",
+      "description": "Galaxia en interacción",
+      "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/HST/AntennaeGalaxies/012.jpg"
+    },
+    "3": {
+      "title": "ARP 142",
+      "description": "Galaxia en interacción",
+      "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/HST/ARP142/012.jpg"
+    },
+    "4": {
+      "title": "ARP 273",
+      "description": "Galaxia en interacción",
+      "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/HST/ARP142/012.jpg"
+    },
+    "5": {
+      "title": "Objetos Herbi Haro",
+      "description": "Región de formacion estelar",
+      "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/HST/HH901/012.jpg"
+    },
+    "6": {
+      "title": "Nebulosa del Aguila",
+      "description": "Región de formacion estelar",
+      "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/HST/M16/012.jpg"
+    },
+    "7": {
+      "title": "M17",
+      "description": "Región de formacion estelar",
+      "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/HST/M17_SFR/012.jpg"
+    },
+    "8": {
+      "title": "NGC 5189",
+      "description": "Nebulosa planetaria",
+      "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/HST/NGC5189/012.jpg"
+    },
+    "9": {
+      "title": "NGC 6357",
+      "description": "Región de formacion estelar",
+      "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/HST/NGC6357/012.jpg"
+    },
+    "10": {
+      "title": "ARP 273",
+      "description": "Nebulosa planetaria",
+      "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/HST/ARP142/012.jpg"
+    },
+    "11": {
+      "title": "Objetos Herbi Haro",
+      "description": "Nebulosa planetaria",
+      "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/HST/HH901/012.jpg"
+    },
+    "12": {
+      "title": "Nebulosa del Aguila",
+      "description": "Nebulosa planetaria",
+      "url": "https://raw.githubusercontent.com/nicomedinap/nicomedinap.github.io/master/Galeria/HST/M16/012.jpg"
+    },
+  }
+};
