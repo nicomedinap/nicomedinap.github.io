2654c88f8f1d5cb24afd18a4c43b499ec3e77c4a
Nicolás Medina Peña <nicomedinap@gmail.com>
2025-12-09 11:07:26 -0300

    Create ciudades.js

---
+++ preboles/ciudades.js
@@ -0,0 +1,28 @@
+/* === CIUDADES === */
+const chileanCities = {
+  "Arica":            { lat: -18.4783, lon: -70.3126, region: "XV Región",  altitude: 30 },
+  "Iquique":          { lat: -20.2146, lon: -70.1524, region: "I Región",   altitude: 56 },
+  "Antofagasta":      { lat: -23.6509, lon: -70.3975, region: "II Región",  altitude: 40 },
+  "Calama":           { lat: -22.4544, lon: -68.9294, region: "II Región",  altitude: 2260 },
+  "Copiapó":          { lat: -27.3667, lon: -70.3333, region: "III Región", altitude: 390 },
+  "La Serena":        { lat: -29.9027, lon: -71.2519, region: "IV Región",  altitude: 28 },
+  "Coquimbo":         { lat: -29.9533, lon: -71.3436, region: "IV Región",  altitude: 15 },
+  "San Felipe":       { lat: -32.7497, lon: -70.7258, region: "V Región",   altitude: 656 },
+  "Valparaíso":       { lat: -33.0458, lon: -71.6197, region: "V Región",   altitude: 40 },
+  "Viña del Mar":     { lat: -33.0245, lon: -71.5518, region: "V Región",   altitude: 20 },
+  "Santiago":         { lat: -33.4489, lon: -70.6693, region: "RM",         altitude: 570 },
+  "Rancagua":         { lat: -34.1701, lon: -70.7406, region: "VI Región",  altitude: 572 },
+  "Talca":            { lat: -35.4264, lon: -71.6554, region: "VII Región", altitude: 102 },
+  "Linares":          { lat: -35.8454, lon: -71.5979, region: "VII Región", altitude: 165 },
+  "Chillán":          { lat: -36.6066, lon: -72.1034, region: "Ñuble",      altitude: 124 },
+  "Concepción":       { lat: -36.8269, lon: -73.0498, region: "VIII Región", altitude: 12 },
+  "Los Ángeles":      { lat: -37.4697, lon: -72.3536, region: "VIII Región", altitude: 139 },
+  "Temuco":           { lat: -38.7359, lon: -72.5907, region: "IX Región",  altitude: 114 },
+  "Valdivia":         { lat: -39.8142, lon: -73.2459, region: "XIV Región", altitude: 19 },
+  "Osorno":           { lat: -40.5739, lon: -73.1310, region: "X Región",   altitude: 65 },
+  "Puerto Montt":     { lat: -41.4689, lon: -72.9411, region: "X Región",   altitude: 85 },
+  "Punta Arenas":     { lat: -53.1638, lon: -70.9171, region: "XII Región", altitude: 34 },
+};
+
+// Exportar ciudades
+window.chileanCities = chileanCities;
